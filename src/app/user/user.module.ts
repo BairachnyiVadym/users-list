4fd1359c90f059e1b7dbd1af5913faf9ccb9d4bb
Vadym Bairachnyi <coworking1b.v@gmail.com>
2019-09-19 14:00:27 +0300

    Created the shared module. Imported, declared and exported the modal component in it. Imported and registered the shared module in the user module.

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,25 +4,25 @@ import { UserRoutingModule } from './user-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { StorageModule } from '@ngx-pwa/local-storage';
+import { SharedModule } from '../shared/shared.module';
 
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { UserAddComponent } from './components/user-add/user-add.component';
-import { ModalComponent } from '../shared/modal/modal.component';
 
 @NgModule({
   declarations: [
     UserListComponent,
     UserProfileComponent,
-    UserAddComponent,
-    ModalComponent
+    UserAddComponent
   ],
   imports: [
     CommonModule,
     UserRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    StorageModule.forRoot({ IDBNoWrap: true })
+    StorageModule.forRoot({ IDBNoWrap: true }),
+    SharedModule
   ]
 })
 export class UserModule { }
